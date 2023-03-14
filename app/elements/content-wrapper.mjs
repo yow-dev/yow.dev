b86090f9e6cb64ebfa2a92239686a35f785ecfea
macdonst <simon.macdonald@gmail.com>
2023-03-14 13:41:19 -0400

    Wrapper for readability
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

---
+++ app/elements/content-wrapper.mjs
@@ -0,0 +1,33 @@
+export default function ContentWrapper({ html }) {
+  return html`
+        <style>
+          :host {
+            display: block;
+            max-width: 20em;
+            margin-left: auto;
+            margin-right: auto;
+          }
+
+      @media screen and (min-width: 30em) {
+        :host {
+          max-width: 30em;
+        }
+      }
+
+      @media screen and (min-width: 48em) {
+        :host {
+          max-width: 40em;
+        }
+      }
+
+      @media screen and (min-width: 90em) {
+        :host {
+          max-width: min(40vw, 50rem);
+        }
+      }
+
+        </style>
+
+        <slot></slot>
+      `
+}
